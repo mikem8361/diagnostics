bcfc46b300b9eb7727a6e75fa42e170fc6d2e529
Mike McLaughlin <mikem@microsoft.com>
2021-03-06 10:49:55 -0800

    Add VS SOS support
    Add VS.sln
    
    Fix native SOS interop to work on desktop CLR.
    
    Move help command to common extension assembly. Fix hard coded Console.WindowWidth usage.
    
    Move read memory command to common extension assembly. Useful to test memory service impl.
    
    Move AssemblyResolve helper class. The VS package needs it to load the right version of
    System.Runtime.CompilerServices.Unsafe (Version 5.0.0.0).
    
    Ignore duplicate Microsoft.NETCore.App.Host.win-x64 [3.1.23] down packages
    
    Clear VS env vars left over from managed build in build-native.cmd
    
    SDK project for VSIX project
    
    Add native SOS binaries to package

---
+++ src/VS/Microsoft.Diagnostics.SOSConcord/SOSConcordServiceMessageId.cs
@@ -0,0 +1,14 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.Diagnostics.SOSConcord
+{
+    /// <summary>
+    /// Messages to the SOS concord service process
+    /// </summary>
+    public enum SOSConcordServiceMessageId
+    {
+        ExecuteCommand,
+        SetContext,
+    }
+}

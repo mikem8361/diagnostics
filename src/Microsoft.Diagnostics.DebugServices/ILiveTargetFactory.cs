16418eab7c5ac0aacd19d796cd6f42c6b38b8c56
Mike McLaughlin <mikem@microsoft.com>
2022-05-29 14:45:01 -0700

    Add multi-target support
    Cleanup and making error cases more bullet proof.
    
    Change the context service ordering to prevent creating runtimes if not needed
    
    Add a common Host.cs that impls IHost.
    
    Add opendump/attach commands.

--- src/Microsoft.Diagnostics.DebugServices/ILiveTargetFactory.cs
+++ src/Microsoft.Diagnostics.DebugServices/ILiveTargetFactory.cs
@@ -6,17 +6,15 @@ using System;
 namespace Microsoft.Diagnostics.DebugServices
 {
     /// <summary>
-    /// Opens and creates minidump and core dump targets.
+    /// Attaches to live processes.
     /// </summary>
-    public interface IDumpTargetFactory
+    public interface ILiveTargetFactory
     {
         /// <summary>
-        /// Opens and creates a dump data target.
+        /// Attaches to a live process and suspends it until the target is destroyed/closed.
         /// </summary>
-        /// <param name="fileName"></param>
         /// <returns>target instance</returns>
         /// <exception cref="DiagnosticsException">can not construct target instance</exception>
-        /// <exception cref="NotSupportedException">target platform not supported</exception>
-        ITarget OpenDump(string fileName);
+        ITarget Attach(int processId);
     }
 }

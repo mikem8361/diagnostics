f0b0d596e958b7a699d3775465af3a0d93a3d409
Mike McLaughlin <mikem@microsoft.com>
2024-03-12 18:52:59 -0700

    Seperate the output functions into separate interface
    INIT_API_EXT macro cleanup: added ExtInit function that does all command entry init.
    
    Change the no managed hosting fallback code (platform/*) to use the IDebuggerServices instance
    instead of the global dbgeng variables because it is always available/setup. This allows the
    CLRMA support to work with no managed code.
    
    Add IOutputService in outputservice.h
    
    Move Strike/platform to extensions/fallback

--- src/SOS/SOS.Extensions/HostServices.cs
+++ src/SOS/SOS.Extensions/HostServices.cs
@@ -149,7 +149,7 @@ namespace SOS.Extensions
 
         public IReadOnlyList<string> ExecuteCommand(string commandLine) => _commandService.ExecuteAndCapture(commandLine, _contextService.Services);
 
-        public IReadOnlyList<string> ExecuteHostCommand(string commandLine) => DebuggerServices.ExecuteHostCommand(commandLine, DEBUG_OUTPUT.NORMAL | DEBUG_OUTPUT.ERROR);
+        public IReadOnlyList<string> ExecuteHostCommand(string commandLine) => DebuggerServices.ExecuteHostCommand(commandLine, normal: true, error: true);
 
         #region IHost
 
@@ -188,7 +188,17 @@ namespace SOS.Extensions
             // Create the wrapper for the host debugger services
             try
             {
-                DebuggerServices = new DebuggerServices(iunk, HostType);
+                DebuggerServices = new(iunk, HostType);
+            }
+            catch (InvalidCastException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return HResult.E_NOINTERFACE;
+            }
+            DebuggerOutputService outputService;
+            try
+            {
+                outputService = new(iunk);
             }
             catch (InvalidCastException ex)
             {
@@ -198,7 +208,7 @@ namespace SOS.Extensions
             HResult hr;
             try
             {
-                ConsoleServiceFromDebuggerServices consoleService = new(DebuggerServices);
+                ConsoleServiceFromDebuggerServices consoleService = new(outputService);
                 FileLoggingConsoleService fileLoggingConsoleService = new(consoleService);
                 DiagnosticLoggingService.Instance.SetConsole(consoleService, fileLoggingConsoleService);
 
